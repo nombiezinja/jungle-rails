7c5c15e0a98f6a2d63f60880f06acb71c06e3c63
Ti Zhang <tizhang8835@gmail.com>
2017-07-06 20:54:25 +0000

    Added user registration/login/logout features

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,25 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :delete, :show]
   end
 
+  # resource :login, only: [:show] do
+  #   root to: 'session#new'
+  #   post :'session#create'
+  # end
+
+  # resource :logout, only: [:show] do
+  #   destroy :'session#destroy'
+  # end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  resource :signup, only: [:show] do
+    root to: 'users#new'
+  end
+
+  resources :users, only: [:create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
