9ba9a03ae12b514b1223e05d6afaf1e7d5110573
Ti Zhang <tizhang8835@gmail.com>
2017-07-07 03:21:16 +0000

    Added fix for checking out with empty cart

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
 
-  http_basic_authenticate_with name: "#{ENV['ADMIN_NAME']}", password: "#{ENV['ADMIN_PASSWORD']}"
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
 
   # def admin_authenticate
   #   super
